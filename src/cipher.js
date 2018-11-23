18683b8ec4cfed9b7e0c1ebbe6e6d383804c2070
cinthyasegura <cinthyaless@gmail.com>
2018-11-23 13:41:15 -0500

    agregue estructura html y funciones

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,34 @@
 window.cipher = {
-  // ... 
+
+  encode: (offset, text) => {
+    const str = text.toUpperCase();
+    let solved = [];
+    for (i=0; i<str.length; i++){
+        const numAscii = str.charCodeAt(i);
+    if (numAscii >= 65 && numAscii <= 90) {
+        const strEncr = String.fromCharCode((numAscii - 65 + offset) % 26 + 65);
+        solved.push(strEncr);
+    } else if (numAscii == 32) {
+        solved.push(" ");
+      }
+    } 
+     return solved.join(""); 
+  },
+
+
+  decode: (offset, text) => {
+    const strEncrypt = text.toUpperCase();
+    let solution = [];
+    for (i=0; i<strEncrypt.length; i++){
+     const asciiNum = strEncrypt.charCodeAt(i);
+    if (asciiNum >= 65 && asciiNum <= 90) {
+      const strDecr = String.fromCharCode((asciiNum - 90 - offset) % 26 + 90);
+      solution.push(strDecr); 
+    } else if (asciiNum == 32) {
+       solution.push(" ");
+      }
+    } 
+     return solution.join(""); 
+  },
+
 };
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const offset = document.getElementById("offset");// si se le pone .value coge como valor inicial = 0
+const box1 = document.getElementById("box-1");
+const box2 = document.getElementById("box-2");
+const btnEncr = document.getElementById("btn-encr");
+const btnDecr = document.getElementById("btn-decr");
+const btnBack = document.getElementById("btn-back");
+const paragraph = document.getElementById("paragraph");
