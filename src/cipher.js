e96655943b38fe82343e7b2df7e105d4692dd241
cinthyasegura <cinthyaless@gmail.com>
2018-11-26 13:06:55 -0500

    cifrado y descifrado de minusculas cambios en tamaño de container

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,19 @@
 window.cipher = {
 
   encode: (offset, string) => {
-    const str = string.toUpperCase();
+    //const str = string.toUpperCase();
     let solved = [];
-    for (i=0; i<str.length; i++){
-        const numAscii = str.charCodeAt(i);
+    for (i=0; i<string.length; i++){
+        const numAscii = string.charCodeAt(i);
     if (numAscii >= 65 && numAscii <= 90) {
         const strEncr = String.fromCharCode((numAscii - 65 + offset) % 26 + 65);
         solved.push(strEncr);
+    } else if (numAscii >= 97 && numAscii <= 122) {
+        const strEncrLowerCase = String.fromCharCode((numAscii - 97 + offset) % 26 + 97);
+        solved.push(strEncrLowerCase);
+        
     } else if (numAscii == 32) {
-        solved.push(" ");
+        solved.push(" "); 
       }
     } 
      return solved.join(""); 
@@ -17,13 +21,17 @@ window.cipher = {
 
 
   decode: (offset, string) => {
-    const strEncrypt = string.toUpperCase();
+    //const strEncrypt = string.toUpperCase();
     let solution = [];
-    for (i=0; i<strEncrypt.length; i++){
-     const asciiNum = strEncrypt.charCodeAt(i);
+    for (i=0; i<string.length; i++){
+     const asciiNum = string.charCodeAt(i);
     if (asciiNum >= 65 && asciiNum <= 90) {
       const strDecr = String.fromCharCode((asciiNum - 90 - offset) % 26 + 90);
       solution.push(strDecr); 
+    } else if(asciiNum >= 97 && asciiNum <= 122) {
+        var strDecrLowerCase = String.fromCharCode((asciiNum - 122 - offset) % 26 + 122);
+      solution.push(strDecrLowerCase);
+    
     } else if (asciiNum == 32) {
        solution.push(" ");
       }
@@ -31,4 +39,4 @@ window.cipher = {
      return solution.join(""); 
   },
 
-};
+}; 
